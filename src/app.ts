569c654a2677cc2594401448ac639730f6aecec6
javascripty <ed2711uribe@gmail.com>
2024-08-27 17:55:30 -0700

    First commit.

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+interface ITodo {
+  description: string;
+  done: boolean;
+}
+
+export class App {
+  heading: string;
+  todos: ITodo[];
+  todoDescription: string;
+
+  constructor() {
+    this.heading = "notes to oneself";
+    this.todos = [];
+    this.todoDescription = "";
+  }
+
+  addTodo(): void {
+    if (this.todoDescription) {
+      this.todos.push({
+        description: this.todoDescription,
+        done: false,
+      });
+      this.todoDescription = "";
+    }
+  }
+
+  removeTodo(todo: ITodo): void {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+}
